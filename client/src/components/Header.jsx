04f9bbc895fe1356f98774dc2630576ff1eba5e1
Mohammad Ibrahim <ibrahim.soft.dev23@gmail.com>
2023-10-29 01:29:01 +0600

    client_side_Setup
    here i update this project client side

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+      <div className='w-full bg-white'>
+          <div className='header-top bg-[#eeeeee]'>
+              
+          </div>
+     
+    </div>
+  )
+}
+
+export default Header
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function Home() {
+  return (
+      <div className='w-full'>
+          <Header/>
+     
+    </div>
+  )
+}
+
+export default Home
