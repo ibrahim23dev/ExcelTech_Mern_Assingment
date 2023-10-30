9e133af93ad434df4d91bef0e134a6ffe3610b39
Mohammad Ibrahim <ibrahim.soft.dev23@gmail.com>
2023-10-30 23:24:45 +0600

    hlw
    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
-
+import { GrMail } from 'react-icons/gr'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai'
+import { Fa500Px, FaFacebook } from 'react-icons/fa'
+import { FaLinkedin} from 'react-icons/fa'
 function Header() {
   return (
       <div className='w-full bg-white'>
-          <div className='header-top bg-[#eeeeee]'>
-              
+      <div className='header-top bg-[#eeeeee] md-lg:hidden'>
+        <div className='w-[85%]  lg:w-[90%] mx-auto'>
+          <div className='flex w-full justify-center items-center h-[50px] text-slate-500'>
+            <ul className='flex justify-start items-center gap-8'>
+              <li className='flex justify-center items-center gap-2 text-sm after:absolute relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px]'>
+                <span><GrMail/></span>
+                <span>ibrahim@gemail.com</span>
+              </li>
+              <span>E-commerce website Whice is Multi Vendor</span>
+            </ul>
+          
+              <div className='flex justify-center items-center gap-10'>
+               <div className='flex justify-center items-center gap-4'>
+                  <a href='#'><FaFacebook/></a>
+                  <a href='#'><AiOutlineTwitter/></a>
+                  <a href='#'><FaLinkedin/></a>
+                   <a href='#'><AiFillGithub/></a>
+             
+             </div>
+            </div>
+          </div>
+        </div>
+             
           </div>
      
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xl': {'max':'1200px'},
+     ' lg': {'max':'1080px'},
+      'md': {'max':'768px'},
+      'md-lg': {'max':'991px'},
+      'xs': {'max':'480px'},
+      'sm': {'max':'576px'},
+      '2xs': { 'max':'340px'}
+    }
   },
   plugins: [],
 }
