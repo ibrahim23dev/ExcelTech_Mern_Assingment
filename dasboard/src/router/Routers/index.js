bd7cb468260ee1af1e3b4b2ebcb63a703425ff60
Mohammad Ibrahim <ibrahim.soft.dev23@gmail.com>
2023-12-06 00:34:42 +0600

    hi
    hlw

--- dasboard/src/router/Routers/index.js
+++ dasboard/src/router/Routers/index.js
@@ -1,10 +1,10 @@
 import { privateRoute } from './privateRouter'
 import MainLayout from '../../Layout/MainLayout'
-import ProtectRoute from './protectRouter'
+//import ProtectRoute from './protectRouter'
 export const GetRoutes = () => {
     const allRoute = []
     privateRoute.map(r => {
-        r.element = <ProtectRoute route={r} >{r.element}</ProtectRoute>
+        //r.element = <ProtectRoute route={r} >{r.element}</ProtectRoute>
     })
     return {
         path: '/',
